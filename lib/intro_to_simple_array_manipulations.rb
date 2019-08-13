8ba94b861017a82f6a5798637dc67d079729dd13
Steven Decker <swdecker@gmail.com>
2019-08-13 20:48:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,9 +22,6 @@ def shift_with_args(arr)
   arr.shift(2)
 end
 
-def using_uniq(arr)
-  arr.uniq
-
 def using_concat(arr1, arr2)
   arr1.concat(arr2)
 end
@@ -33,6 +30,12 @@ def using_insert(arr, elem)
   arr.insert(4,elem)
 end
 
+def using_uniq(arr)
+  arr.uniq!
+end
+
+def using_flatten(arr)
+  
 
 
 
